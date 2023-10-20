4cc396c110d93f6e5152b5521a3c7d3f0d9b764f
Martin <marvahus@proton.me>
2023-10-21 00:58:56 +0200

    Added kernel/mem.h
    This included some functions from <string.h> like strlen, memmove, memset

---
+++ kernel/mem.h
@@ -0,0 +1,27 @@
+#include <stddef.h>
+#if !defined (KERNEL_MEM_H)
+#define KERNEL_MEM_H
+
+#include <stdint.h>
+
+void memmove(void* dest, const void* src, size_t count) {
+    for (size_t i = 0; i < count; ++i) {
+        ((uint8_t*) dest)[i] = ((uint8_t*) src)[i];
+    }
+}
+
+void memset(void* dest, uint8_t val, size_t count) {
+    for (size_t i = 0; i < count; ++i) {
+        ((uint8_t*) dest)[i] = val;
+    }
+}
+
+size_t strlen(const char* str) {
+    size_t len = 0;
+    while (str[len]) {
+        len++;
+    }
+    return len;
+}
+
+#endif

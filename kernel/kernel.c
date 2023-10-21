f89fc3b1226ecbce265654e8e6b85a938b4d8ff7
Martin <marvahus@proton.me>
2023-10-21 22:43:28 +0200

    Multiboot Magic, and Multiboot Info

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -5,6 +5,7 @@
 #include "../include/defs.h"
 #include "../include/types.h"
 #include "./vga_text.h"
+#include "./multiboot.h"
 
 #if defined (__linux__)
 #error "You are not using a cross-compiler, you will most certainly run into trouble"
@@ -14,35 +15,32 @@
 #error "This needs to be compiled with a ix86-elf compiler"
 #endif
 
-struct MultibootInfo {
-    u32 flags;
-    u32 mem_lower;
-    u32 mem_upper;
-    // Other stuff
-};
 
-void kernel_main(u32 multiboot_magic, const struct MultibootInfo* info) {
-    terminal_initialize();
-
+internal
+void check_multiboot_magic(u32 multiboot_magic) {
     terminal_put_hex((u8*) &multiboot_magic, sizeof(u32), true);
     terminal_putchar(' ');
     terminal_writestring(
         (multiboot_magic == 0x2BADB002) 
-        ? "VALID"
-        : "INVALID"
+        ? (u8*) "VALID"
+        : (u8*) "INVALID"
     );
-    terminal_writestring(" multiboot magic\n");
+    terminal_writestring((u8*) " multiboot magic\n");
+}
 
-    terminal_put_u32(info->flags);
-    terminal_putchar('\n');
+internal
+void check_boot_loader_name(const struct Multiboot_Info* info) {
+    if (info->flags & Boot_Loader_Name) {
+        terminal_writestring((u8*) "Has Name: '");
+        terminal_writestring((u8*) info->boot_loader_name);
+        terminal_writestring((u8*) "'\n");
+    } else {
+        terminal_writestring((u8*) "Doesn't Have Name\n");
+    }
+}
 
-    terminal_put_u32(info->mem_lower);
-    terminal_putchar('\n');
-
-    terminal_put_u32(info->mem_upper);
-    terminal_putchar('\n');
-
-//    terminal_writestring("Hello, Kernel!\n");
-//    terminal_writestring("the quick brown fox jumped over the lazy dog\n");
-//    terminal_writestring("THE QUICK BROWN FOX JUMPED OVER THE LAZY DOG\n");
+void kernel_main(u32 multiboot_magic, const struct Multiboot_Info* info) {
+    terminal_initialize();
+    check_multiboot_magic(multiboot_magic);
+    check_boot_loader_name(info);
 }

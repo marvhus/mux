d9b7b14a32e0a81142eefd667043f2c42bc12b07
Martin <marvahus@proton.me>
2023-10-21 20:13:41 +0200

    Updated kernel/mem.h
    Replaced data types with the custom ones in include/types.h

--- kernel/mem.h
+++ kernel/mem.h
@@ -2,23 +2,23 @@
 #if !defined (KERNEL_MEM_H)
 #define KERNEL_MEM_H
 
-#include <stdint.h>
+#include "../include/types.h"
 
-void memmove(void* dest, const void* src, size_t count) {
-    for (size_t i = 0; i < count; ++i) {
-        ((uint8_t*) dest)[i] = ((uint8_t*) src)[i];
+void memmove(void* dest, const void* src, size count) {
+    for (size i = 0; i < count; ++i) {
+        ((u8*) dest)[i] = ((u8*) src)[i];
     }
 }
 
 // @TODO: figure out a solution for situations where the values are larger than a byte.
-void memset(void* dest, uint8_t val, size_t count) {
-    for (size_t i = 0; i < count; ++i) {
-        ((uint8_t*) dest)[i] = val;
+void memset(void* dest, u8 val, size count) {
+    for (size i = 0; i < count; ++i) {
+        ((u8*) dest)[i] = val;
     }
 }
 
-size_t strlen(const char* str) {
-    size_t len = 0;
+size strlen(const u8* str) {
+    size len = 0;
     while (str[len]) {
         len++;
     }
